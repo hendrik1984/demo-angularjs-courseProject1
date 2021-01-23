8c5dccc367a63c721c54d1dc6727d1602811d62d
Hendrik Rahardja <hendrik@alterra.id>
2021-01-23 16:49:40 +0700

    update sample recipe

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -14,9 +14,9 @@ export class RecipeListComponent implements OnInit {
       'This a simple a test',
       'https://img.freepik.com/free-photo/juicy-steak-rare-beef-with-spices-wooden-board-garnish-asparagus_2829-1513.jpg?size=626&ext=jpg'),
       new Recipe(
-        'A test Recipe',
-        'This a simple a test',
-        'https://img.freepik.com/free-photo/juicy-steak-rare-beef-with-spices-wooden-board-garnish-asparagus_2829-1513.jpg?size=626&ext=jpg')
+        'A test Recipe 2',
+        'This a simple a test 2',
+        'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/marinated-onglet_0-497a617.jpg?webp=true&quality=90&resize=620%2C310')
     ];
 
   constructor() { }
