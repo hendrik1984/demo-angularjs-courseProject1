5b88408c0225a43bfa5736042182fe0abc2a8449
Hendrik Rahardja <hendrik@alterra.id>
2021-02-07 17:42:39 +0700

    add shopping list service

--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -1,3 +1,27 @@
-export class ShoppingListService {
+import { EventEmitter } from "@angular/core";
+import { Ingredient } from "../shared/ingredient.model";
 
+export class ShoppingListService {
+    ingredientsChanged = new EventEmitter<Ingredient[]>();
+    private ingredients: Ingredient[] = [
+        new Ingredient('Apple', 5),
+        new Ingredient('Tomatoes', 10),
+    ];
+
+    getIngredients() {
+        return this.ingredients.slice();
+        // sometimes when you add need to check if the array data is slice/copy or not,
+        // if its slice/copy you cant add because you add data into copy array not the reference one
+
+        // return this.ingredients;
+        // the simple way to add is using reference data instead copy
+        // this part related to shopping list add content
+    }
+
+    addIngredient(ingredient: Ingredient) {
+        this.ingredients.push(ingredient);
+
+        // if you use getIngredients slice() need to emit data in here
+        this.ingredientsChanged.emit(this.ingredients.slice());
+    }
 }
