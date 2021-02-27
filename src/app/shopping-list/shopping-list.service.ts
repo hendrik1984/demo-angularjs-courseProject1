997e35ddd9f1fb3269d7c6c310103e8203dac982
Hendrik Rahardja <hendrik@alterra.id>
2021-02-27 22:38:16 +0700

    change EventEmitter using Subject, remove unused code on recipe

--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -1,8 +1,9 @@
 import { EventEmitter } from "@angular/core";
+import { Subject } from "rxjs";
 import { Ingredient } from "../shared/ingredient.model";
 
 export class ShoppingListService {
-    ingredientsChanged = new EventEmitter<Ingredient[]>();
+    ingredientsChanged = new Subject<Ingredient[]>();
     private ingredients: Ingredient[] = [
         new Ingredient('Apple', 5),
         new Ingredient('Tomatoes', 10),
@@ -22,7 +23,7 @@ export class ShoppingListService {
         this.ingredients.push(ingredient);
 
         // if you use getIngredients slice() need to emit data in here
-        this.ingredientsChanged.emit(this.ingredients.slice());
+        this.ingredientsChanged.next(this.ingredients.slice());
     }
 
     addIngredients(ingredients: Ingredient[]) {
@@ -31,6 +32,6 @@ export class ShoppingListService {
         // }
 
         this.ingredients.push(...ingredients);
-        this.ingredientsChanged.emit(this.ingredients.slice());
+        this.ingredientsChanged.next(this.ingredients.slice());
     }
 }
