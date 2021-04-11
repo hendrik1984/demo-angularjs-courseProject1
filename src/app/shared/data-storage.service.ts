ac0e315920cf0739197f5f32eca6310233062813
Hendrik Rahardja <hendrik@alterra.id>
2021-04-11 22:02:02 +0700

    add function fetch data from firebase

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
 import { RecipeService } from "../recipes/recipe.service";
+import { Recipe } from "../recipes/recipe.model";
+import { map } from "rxjs/operators";
 
 @Injectable({providedIn: 'root'})
 export class DataStorageService {
@@ -16,5 +18,19 @@ export class DataStorageService {
         });
     }
 
+    fetchRecipes() {
+        this.http
+        .get<Recipe[]>(
+            'https://ng-course-recipe-book-bb29e-default-rtdb.firebaseio.com/recipes.json'
+        ).pipe(map(recipes => {
+            return recipes.map(recipe => {
+                return {...recipe, ingredients: recipe.ingredients ? recipe.ingredients : []}
+            });
+        }))
+        .subscribe(recipes => {
+            // console.log(recipes);
+            this.recipeService.setRecipes(recipes);
+        });
+    }
 
 }
