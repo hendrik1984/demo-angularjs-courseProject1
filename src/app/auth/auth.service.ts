dcf18a3d218c4b76bc463d2c7a203665eec1bceb
Hendrik Rahardja <hendrik@alterra.id>
2021-04-13 21:46:40 +0700

    login and sign up merge logic code

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -3,14 +3,14 @@ import { Injectable } from "@angular/core";
 import { catchError } from "rxjs/operators";
 import { throwError } from "rxjs";
 
-interface AuthResponseData {
+export interface AuthResponseData {
     kind: string;
     idToken: string;
     email: string;
     refreshToken: string;
     expiresIn: string;
     localId: string;
-
+    registered?: boolean;
 }
 
 @Injectable({providedIn: 'root'})
@@ -25,16 +25,29 @@ export class AuthService {
                 password: password,
                 returnSecureToken: true
             }
-        ).pipe(catchError(errorRes => {
-            let errorMessage = "An unknown error occurred!";
-            if (!errorRes.error || !errorRes.error.error) {
+        ).pipe(
+            catchError(errorRes => {
+                let errorMessage = "An unknown error occurred!";
+                if (!errorRes.error || !errorRes.error.error) {
+                    return throwError(errorMessage);
+                }
+                switch (errorRes.error.error.message) {
+                    case 'EMAIL_EXISTS':
+                        errorMessage = 'This email exists already';
+                }
                 return throwError(errorMessage);
+            })
+        );
+    }
+
+    login(email: string, password: string) {
+        return this.http.post<AuthResponseData>(
+            'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyBhlwr8vvs8NmbcojtL-q9AaK1v7cesEoo',
+            {
+                email: email,
+                password: password,
+                returnSecureToken: true
             }
-            switch (errorRes.error.error.message) {
-                case 'EMAIL_EXISTS':
-                    errorMessage = 'This email exists already';
-            }
-            return throwError(errorMessage);
-        }));
+        );
     }
 }
