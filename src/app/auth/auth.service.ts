17adfa95d9becc3aab48c0ee928a358c30c81b0f
Hendrik Rahardja <hendrik@alterra.id>
2021-04-19 22:59:43 +0700

    add routing and guard it

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -18,7 +18,7 @@ export interface AuthResponseData {
 @Injectable({providedIn: 'root'})
 export class AuthService {
     user = new BehaviorSubject<User>(null);
-    token = null;
+    private tokenExpirationTimer: any;
 
     constructor(private http: HttpClient, private route: Router) {}
 
@@ -64,9 +64,45 @@ export class AuthService {
         );
     }
 
+    autoLogin() {
+        const userData: {
+            email: string,
+            id: string,
+            _token: string,
+            _tokenExpirationDate: string;
+        } = JSON.parse(localStorage.getItem('userData'));
+        if (!userData) {
+            return;
+        }
+
+        const loadedUser = new User(
+            userData.email,
+            userData.id,
+            userData._token,
+            new Date(userData._tokenExpirationDate)
+        );
+
+        if (loadedUser.token) {
+            this.user.next(loadedUser);
+            const expirationDuration = new Date(userData._tokenExpirationDate).getTime() - new Date().getTime();
+            this.autoLogout(expirationDuration);
+        }
+    }
+
     logout() {
         this.user.next(null);
         this.route.navigate(['/auth']);
+        localStorage.removeItem('userData');
+        if (this.tokenExpirationTimer) {
+            clearTimeout(this.tokenExpirationTimer);
+        }
+    }
+
+    autoLogout(expirationDuration: number) {
+        console.log(expirationDuration);
+        this.tokenExpirationTimer = setTimeout(() => {
+            this.logout();
+        }, expirationDuration);
     }
 
     private handleAuthentication(
@@ -85,6 +121,8 @@ export class AuthService {
         );
 
         this.user.next(user);
+        this.autoLogout(expiresIn * 1000);
+        localStorage.setItem('userData', JSON.stringify(user));
     }
 
     private handleError(errorRes: HttpErrorResponse) {
